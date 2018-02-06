347542701a8e47b94c7bc636db74bfdd3d51fa67
Lars Bohm <larsbohm@gmail.com>
2018-02-06 17:49:52 +0100

    added javascript

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -3,3 +3,21 @@
 //= require_tree .
 
 
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  const currentDate = new Date;
+  const textToChange = document.getElementById('beer-time');
+  const beerImage = textToChange.dataset.beer;
+  const programmingImage = textToChange.dataset.programming;
+
+
+  if (currentDate.getHours() > 18) {
+    textToChange.innerText = "YES";
+    document.body.style.backgroundImage = `url(${beerImage})`;
+  } else {
+    textToChange.innerText = "NO";
+    document.body.style.backgroundImage = `url(${programmingImage})`;
+  }
+
+});
