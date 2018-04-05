939c11b4a28855a15a36aee6940f2451b68bb9ec
Lars Bohm <larsbohm@gmail.com>
2018-04-05 15:22:25 +0200

    add stuff

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -3,21 +3,40 @@
 //= require_tree .
 
 
+function getRandomGiphy(tag) {
+  fetch("http://api.giphy.com/v1/gifs/random?api_key=mdOEurpRKHYZZilfsXO7DV1vbdDsZSdU&tag=" + tag)
+  .then(function(response) { return response.json() })
+  .then(function(data) {
+    console.log(data.data.image_url);
+    document.body.style.backgroundImage = "url(" + data.data.image_url + ")";
+  });
+}
+
+function getFromSoundcloud(tag) {
+  SC.initialize({
+    client_id: 'YOUR_CLIENT_ID'
+  });
+
+  // find all sounds of buskers licensed under 'creative commons share alike'
+  SC.get('/tracks', {
+    q: 'buskers', license: 'cc-by-sa'
+  }).then(function(tracks) {
+    console.log(tracks);
+  });
+}
+
 
 document.addEventListener("DOMContentLoaded", function(event) {
-
   var currentDate = new Date;
   var textToChange = document.getElementById('beer-time');
-  var beerImage = textToChange.dataset.beer;
-  var programmingImage = textToChange.dataset.programming;
 
 
   if (currentDate.getHours() >= 18) {
     textToChange.innerText = "YES";
-    document.body.style.backgroundImage = "url(" + beerImage + ")";
+    getRandomGiphy("beer") 
   } else {
     textToChange.innerText = "NO";
-    document.body.style.backgroundImage = "url(" + programmingImage + ")";
+    getRandomGiphy("boring") 
   }
 
 });
